07d16cf33c12f3fa2091d743a83eead049b371fb
강민준 <kang.minjun@yonsei.ac.kr>
2024-05-07 17:10:17 +0900

    add sys.path in wsgi.py

--- api_pjt/wsgi.py
+++ api_pjt/wsgi.py
@@ -7,10 +7,11 @@ For more information on this file, see
 https://docs.djangoproject.com/en/4.2/howto/deployment/wsgi/
 """
 
-import os
+import os, sys
 
 from django.core.wsgi import get_wsgi_application
 
+sys.path.append('<PATH_TO_MY_DJANGO_PROJECT>/hellodjango')
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "api_pjt.settings")
 
 application = get_wsgi_application()
